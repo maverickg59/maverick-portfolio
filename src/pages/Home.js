cf2d96c82e702b907174c352422eef074f6e7d63
Christopher White <christopher.evan.white88@gmail.com>
2021-07-13 23:29:53 -0700

    first updates to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 function Home() {
-  return <div>This is my motherfucking home page!</div>
+  return (
+    <div>
+      <span>
+        The rest of my site is coming, I promise. I've been busy creating
+        awesome stuff.
+      </span>
+    </div>
+  )
 }
 
 export default Home
